03f1cc24203c094e38db827f8d99009d35cdb469
Bastian Gebhardt <Bastian.Gebhardt@jambit.com>
2019-09-16 00:27:00 +0200

    Format ino file
    * based on arduino plugin of vs code

--- lightwait/lightwait.ino
+++ lightwait/lightwait.ino
@@ -20,68 +20,80 @@ int startChar = 0;
 bool isOn = false;
 unsigned long lastBlink = 0;
 
-void setup() {
-    Serial.begin(9600);
-    pinMode(RED_PIN, OUTPUT);
-    pinMode(GREEN_PIN, OUTPUT);
-    pinMode(BLUE_PIN, OUTPUT);
-    // write the ready signal
-    Serial.write("r");
-  
-    digitalWrite(RED_PIN, zeroValue);
-    digitalWrite(GREEN_PIN, zeroValue);
-    digitalWrite(BLUE_PIN, zeroValue);
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(RED_PIN, OUTPUT);
+  pinMode(GREEN_PIN, OUTPUT);
+  pinMode(BLUE_PIN, OUTPUT);
+  // write the ready signal
+  Serial.write("r");
+
+  digitalWrite(RED_PIN, zeroValue);
+  digitalWrite(GREEN_PIN, zeroValue);
+  digitalWrite(BLUE_PIN, zeroValue);
 }
 
-void loop() {
-    while (Serial.available() > 0) {
-      String inn = Serial.readString();
-      
-      if (inn.charAt(0) == 'b') {
-        isBlinking = true;
-        startChar = 1;
-      } else {
-        isBlinking = false;
-        startChar = 0;
-      }
+void loop()
+{
+  while (Serial.available() > 0)
+  {
+    String inn = Serial.readString();
 
-      int separatorIndex = inn.indexOf(':');
-      int separatorIndex2 = inn.indexOf(':', separatorIndex + 1);
-
-      String rString = inn.substring(startChar, separatorIndex);
-      String gString = inn.substring(separatorIndex + 1, separatorIndex2);
-      String bString = inn.substring(separatorIndex2 + 1);
-
-      red = map(rString.toInt(), 0, 255, 0, OUTPUT_MAX_VALUE);
-      green = map(gString.toInt(), 0, 255, 0, OUTPUT_MAX_VALUE);
-      blue = map(bString.toInt(), 0, 255, 0, OUTPUT_MAX_VALUE);
-
-      if (IS_COMMON_ANODE) {
-          red = map(red, 0, OUTPUT_MAX_VALUE, OUTPUT_MAX_VALUE, 0);
-          green = map(green, 0, OUTPUT_MAX_VALUE, OUTPUT_MAX_VALUE, 0);
-          blue = map(blue, 0, OUTPUT_MAX_VALUE, OUTPUT_MAX_VALUE, 0);
-      }
-
-      digitalWrite(RED_PIN, red);
-      digitalWrite(GREEN_PIN, green);
-      digitalWrite(BLUE_PIN, blue);
+    if (inn.charAt(0) == 'b')
+    {
+      isBlinking = true;
+      startChar = 1;
     }
-  
-  if (isBlinking) {
+    else
+    {
+      isBlinking = false;
+      startChar = 0;
+    }
+
+    int separatorIndex = inn.indexOf(':');
+    int separatorIndex2 = inn.indexOf(':', separatorIndex + 1);
+
+    String rString = inn.substring(startChar, separatorIndex);
+    String gString = inn.substring(separatorIndex + 1, separatorIndex2);
+    String bString = inn.substring(separatorIndex2 + 1);
+
+    red = map(rString.toInt(), 0, 255, 0, OUTPUT_MAX_VALUE);
+    green = map(gString.toInt(), 0, 255, 0, OUTPUT_MAX_VALUE);
+    blue = map(bString.toInt(), 0, 255, 0, OUTPUT_MAX_VALUE);
+
+    if (IS_COMMON_ANODE)
+    {
+      red = map(red, 0, OUTPUT_MAX_VALUE, OUTPUT_MAX_VALUE, 0);
+      green = map(green, 0, OUTPUT_MAX_VALUE, OUTPUT_MAX_VALUE, 0);
+      blue = map(blue, 0, OUTPUT_MAX_VALUE, OUTPUT_MAX_VALUE, 0);
+    }
+
+    digitalWrite(RED_PIN, red);
+    digitalWrite(GREEN_PIN, green);
+    digitalWrite(BLUE_PIN, blue);
+  }
+
+  if (isBlinking)
+  {
     unsigned long currentMillis = millis();
-    
-    if (currentMillis - lastBlink > BLINK_INTERVAL) {
-        if (isOn) {
-            digitalWrite(RED_PIN, zeroValue);
-            digitalWrite(GREEN_PIN, zeroValue);
-            digitalWrite(BLUE_PIN, zeroValue);
-        } else {
-            digitalWrite(RED_PIN, red);
-            digitalWrite(GREEN_PIN, green);
-            digitalWrite(BLUE_PIN, blue);
-        }
-        isOn = !isOn;
-        lastBlink = currentMillis;
+
+    if (currentMillis - lastBlink > BLINK_INTERVAL)
+    {
+      if (isOn)
+      {
+        digitalWrite(RED_PIN, zeroValue);
+        digitalWrite(GREEN_PIN, zeroValue);
+        digitalWrite(BLUE_PIN, zeroValue);
+      }
+      else
+      {
+        digitalWrite(RED_PIN, red);
+        digitalWrite(GREEN_PIN, green);
+        digitalWrite(BLUE_PIN, blue);
+      }
+      isOn = !isOn;
+      lastBlink = currentMillis;
     }
   }
 }
